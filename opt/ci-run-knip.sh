88a42526b276d6a53955954903e310997812db4a
thoroc <thomas.a.roche@gmail.com>
2025-07-01 16:23:06 +0100

    fix: update knip command to include no-exit-code option for better error handling

--- opt/ci-run-knip.sh
+++ opt/ci-run-knip.sh
@@ -2,8 +2,8 @@
 
 # This script is used to run knip to check for unused dependencies
 # and update package.json and package-lock.json accordingly.
-./node_modules/.bin/knip --fix --config .knip.json
-git add package.json package-lock.json --force
+./node_modules/.bin/knip --fix --no-exit-code
+git add . --force
 # disable lefthook for the next command
 LEFTHOOK=0
 git commit -m "ci: update dependencies" --no-verify
