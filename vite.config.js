77974c60f5eaef213adf327f568b1d6b4fd059c3
thoroc <thomas.a.roche@gmail.com>
2025-07-01 23:45:32 +0100

    removed node_modules and moving to vitejs

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'public',
+  build: {
+    outDir: '.',
+    emptyOutDir: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'public/index.html'),
+      },
+      output: {
+        entryFileNames: 'scripts/bundle.js',
+        assetFileNames: 'styles/[name][extname]',
+      },
+    },
+  },
+  server: {
+    open: '/index.html',
+    port: 8080,
+  },
+});
