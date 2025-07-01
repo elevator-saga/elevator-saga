e0f5fc4cf98b77c4e794a95dcfbea93786b33793
thoroc <thomas.a.roche@gmail.com>
2025-07-01 16:04:51 +0100

    refactor: reorganize import statements and enhance fitness worker test coverage

--- src/app/fitness/fitness-worker.js
+++ src/app/fitness/fitness-worker.js
@@ -1,17 +1,21 @@
-importScripts(
-  '../../models/base.js',
-  '../../models/movable.js',
-  '../../models/floor.js',
-  '../../models/user.js',
-  '../../models/elevator.js',
-  '../../models/interfaces.js',
-  '../../models/world.js',
-  './calculate-fitness.js',
-  './do-fitness-suite.js',
-  './fitness-suite.js',
-  './make-average-result.js',
-  './require-nothing.js'
-);
+import { doFitnessSuite } from './do-fitness-suite.js';
+
+if (typeof importScripts === 'function') {
+  importScripts(
+    '../../models/base.js',
+    '../../models/movable.js',
+    '../../models/floor.js',
+    '../../models/user.js',
+    '../../models/elevator.js',
+    '../../models/interfaces.js',
+    '../../models/world.js',
+    './calculate-fitness.js',
+    './do-fitness-suite.js',
+    './fitness-suite.js',
+    './make-average-result.js',
+    './require-nothing.js'
+  );
+}
 
 export const onmessage = function (msg) {
   // Assume it is a code object that should be fitness-tested
--- src/app/fitness/fitness-worker.test.js
+++ src/app/fitness/fitness-worker.test.js
@@ -1,3 +1,6 @@
+const { onmessage } = require('./fitness-worker.js');
+const fitnessWorker = require('./fitness-worker.js');
+
 global.importScripts = jest.fn();
 global.onmessage = jest.fn();
 global.postMessage = jest.fn();
@@ -8,6 +11,10 @@ global.Worker = class {
   onmessage() {}
 };
 
+jest.mock('./do-fitness-suite', () => ({
+  doFitnessSuite: jest.fn(() => ({ score: 42 })),
+}));
+
 describe('fitness-worker', () => {
   afterAll(() => {
     delete global.Worker;
@@ -16,5 +23,20 @@ describe('fitness-worker', () => {
   it('should load without error', () => {
     require('./fitness-worker.js');
     expect(global.importScripts).toHaveBeenCalled();
+  });
+
+  it('should define onmessage handler', () => {
+    expect(typeof onmessage).toBe('function');
+  });
+
+  it('should call doFitnessSuite and postMessage with results', () => {
+    const codeStr = 'some code';
+    const msg = { data: codeStr };
+
+    fitnessWorker.onmessage(msg);
+
+    const { doFitnessSuite } = require('./do-fitness-suite');
+    expect(doFitnessSuite).toHaveBeenCalledWith(codeStr, 6);
+    expect(global.postMessage).toHaveBeenCalledWith({ score: 42 });
   });
 });
